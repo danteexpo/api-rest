1a61f022223bbdbe8dbb8704359d0535b043c02e
Dante <dante0im@gmail.com>
2023-10-26 20:38:19 -0300

    Added CORS

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,18 @@
-import express from "express";
-import router from "./routes/diaries";
+import express from 'express';
+import cors from 'cors';
+import router from './routes/diaries';
+
+const PORT = 8080;
 
 const app = express();
 
 // middleware that transforms req.body into json
 app.use(express.json());
 
-const PORT = 8080;
+app.use(cors());
 
-app.get("/ping", (_req, res) => {
-	console.log("ping!");
-	res.send("pong!");
-});
+app.use('/api/diaries', router);
 
 app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
-
-app.use("/api/diaries", router);
