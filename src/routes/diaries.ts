ea45b3853e5f4062a060843009579969887e6142
Dante <dante0im@gmail.com>
2023-11-08 00:24:30 -0300

    Added DELETE

--- src/routes/diaries.ts
+++ src/routes/diaries.ts
@@ -1,34 +1,39 @@
-import express from "express";
-import * as services from "../services/diaries";
-import toNewDiary from "../utils";
+import express from 'express';
+import * as services from '../services/diaries';
+import toNewDiary from '../utils';
 
 const router = express.Router();
 
-router.get("/", (_req, res) => {
-	res.send(services.getDiaries());
+router.get('/', (_req, res) => {
+  res.send(services.getDiaries());
 });
 
-router.get("/:id", (req, res) => {
-	const diary = services.getDiary(Number(req.params.id));
-	return diary != null ? res.send(diary) : res.sendStatus(404);
+router.get('/:id', (req, res) => {
+  const diary = services.getDiary(Number(req.params.id));
+  return diary != null ? res.send(diary) : res.sendStatus(404);
 });
 
-router.get("/without", (_req, res) => {
-	res.send(services.getDiariesWithoutComments());
+router.get('/without', (_req, res) => {
+  res.send(services.getDiariesWithoutComments());
 });
 
-router.post("/", (req, res) => {
-	try {
-		const newDiary = toNewDiary(req.body);
+router.post('/', (req, res) => {
+  try {
+    const newDiary = toNewDiary(req.body);
 
-		const Diary = services.postDiaries(newDiary);
+    const Diary = services.postDiaries(newDiary);
 
-		res.json(Diary);
-	} catch (e) {
-		if (e instanceof Error) {
-			res.status(400).send(e.message);
-		}
-	}
+    res.json(Diary);
+  } catch (e) {
+    if (e instanceof Error) {
+      res.status(400).send(e.message);
+    }
+  }
+});
+
+router.delete('/:id', (req, res) => {
+  const diary = services.deleteDiary(Number(req.params.id));
+  return diary != null ? res.send(diary) : res.sendStatus(404);
 });
 
 export default router;
--- src/services/diaries.ts
+++ src/services/diaries.ts
@@ -1,33 +1,45 @@
-import { Diary, DiaryWithoutComment, DiaryWithoutId } from "../types";
-import diariesData from "./diariesData.json";
+import { Diary, DiaryWithoutComment, DiaryWithoutId } from '../types';
+import diariesData from './diariesData.json';
 
 const diaries: Diary[] = diariesData as Diary[];
 
 export const getDiaries = (): Diary[] => diaries;
 
 export const getDiariesWithoutComments = (): DiaryWithoutComment[] =>
-	diaries.map(diary => {
-		const { comment, ...localDiary } = diary;
-		return localDiary;
-	});
+  diaries.map((diary) => {
+    const { comment, ...localDiary } = diary;
+    return localDiary;
+  });
 
 export const getDiary = (id: number): Diary | undefined => {
-	const diary = diaries.find(diary => diary.id === id);
-	return diary;
+  const diary = diaries.find((diary) => diary.id === id);
+  return diary;
 };
 
 export const postDiaries = (newDiaryInfo: DiaryWithoutId): Diary => {
-	const { date, weather, visibility, comment } = newDiaryInfo;
+  const { date, weather, visibility, comment } = newDiaryInfo;
 
-	const newDiary = {
-		id: Math.max(...diaries.map(d => d.id)) + 1,
-		date,
-		weather,
-		visibility,
-		comment,
-	};
+  const newDiary = {
+    id: Math.max(...diaries.map((d) => d.id)) + 1,
+    date,
+    weather,
+    visibility,
+    comment,
+  };
 
-	diaries.push(newDiary);
+  diaries.push(newDiary);
 
-	return newDiary;
+  return newDiary;
+};
+
+export const deleteDiary = (id: number): Diary | undefined => {
+  const deletedDiary = diaries.find((diary) => diary.id === id);
+
+  if (!deletedDiary) return undefined;
+
+  const indexOfDeletedDiary = diaries.indexOf(deletedDiary);
+
+  diaries.splice(indexOfDeletedDiary, 1);
+
+  return deletedDiary;
 };
