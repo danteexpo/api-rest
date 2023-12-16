5ccc4a3bbace0c7af2a241cf0cbf8cf479b5c16d
Dante <dante0im@gmail.com>
2023-12-16 14:43:58 -0300

    Formatting

--- src/routes/diaries.ts
+++ src/routes/diaries.ts
@@ -1,53 +1,53 @@
-import express from 'express';
-import * as services from '../services/diaries';
-import { toNewDiary, toUpdatedDiary } from '../utils';
+import express from "express";
+import * as services from "../services/diaries";
+import { toNewDiary, toUpdatedDiary } from "../utils";
 
 const router = express.Router();
 
-router.get('/', (_req, res) => {
-  res.send(services.getDiaries());
+router.get("/", (_req, res) => {
+	res.send(services.getDiaries());
 });
 
-router.get('/:id', (req, res) => {
-  const diary = services.getDiary(Number(req.params.id));
-  return diary != null ? res.send(diary) : res.sendStatus(404);
+router.get("/:id", (req, res) => {
+	const diary = services.getDiary(Number(req.params.id));
+	return diary != null ? res.send(diary) : res.sendStatus(404);
 });
 
-router.get('/without', (_req, res) => {
-  res.send(services.getDiariesWithoutComments());
+router.get("/without", (_req, res) => {
+	res.send(services.getDiariesWithoutComments());
 });
 
-router.post('/', (req, res) => {
-  try {
-    const newDiary = toNewDiary(req.body);
+router.post("/", (req, res) => {
+	try {
+		const newDiary = toNewDiary(req.body);
 
-    const Diary = services.postDiaries(newDiary);
+		const Diary = services.postDiaries(newDiary);
 
-    res.json(Diary);
-  } catch (e) {
-    if (e instanceof Error) {
-      res.status(400).send(e.message);
-    }
-  }
+		res.json(Diary);
+	} catch (e) {
+		if (e instanceof Error) {
+			res.status(400).send(e.message);
+		}
+	}
 });
 
-router.put('/:id', (req, res) => {
-  try {
-    const updatedDiary = toUpdatedDiary(req.body);
+router.put("/:id", (req, res) => {
+	try {
+		const updatedDiary = toUpdatedDiary(req.body);
 
-    const Diary = services.updateDiary(updatedDiary);
+		const Diary = services.updateDiary(updatedDiary);
 
-    res.json(Diary);
-  } catch (e) {
-    if (e instanceof Error) {
-      res.status(400).send(e.message);
-    }
-  }
+		res.json(Diary);
+	} catch (e) {
+		if (e instanceof Error) {
+			res.status(400).send(e.message);
+		}
+	}
 });
 
-router.delete('/:id', (req, res) => {
-  const diary = services.deleteDiary(Number(req.params.id));
-  return diary != null ? res.send(diary) : res.sendStatus(404);
+router.delete("/:id", (req, res) => {
+	const diary = services.deleteDiary(Number(req.params.id));
+	return diary != null ? res.send(diary) : res.sendStatus(404);
 });
 
 export default router;
