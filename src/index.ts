e158d77c340b49c265f83bb188258051139d0931
Dante <dante0im@gmail.com>
2023-12-16 15:30:10 -0300

    Formatting

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import express from 'express';
-import cors from 'cors';
-import router from './routes/diaries';
+import express from "express";
+import cors from "cors";
+import router from "./routes/diaries";
 
 const PORT = 8080;
 
@@ -11,8 +11,8 @@ app.use(express.json());
 
 app.use(cors());
 
-app.use('/api/diaries', router);
+app.use("/api/diaries", router);
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+	console.log(`Server running on port ${PORT}`);
 });
